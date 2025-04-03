0aed0f3aa25d1c1059a06676862bf9e7cd5969b8
Miles <miles.stoudenmire@gmail.com>
2025-04-03 11:50:05 -0400

    Add support for user callbacks

--- src/alternating_update.jl
+++ src/alternating_update.jl
@@ -1,23 +1,39 @@
 
-function sweep_callback(problem; outputlevel, sweep, kws...)
+default_region_callback(problem; kws...) = nothing
+
+default_sweep_callback(problem; kws...) = nothing
+
+function default_sweep_printer(problem; outputlevel, sweep, kws...)
   if outputlevel >= 1
     println("Done with sweep $sweep")
-    #print(" cpu_time=", round(sweep_time; digits=3))
-    #println()
+    #println(" CPU time=", round(sweep_time; digits=3))
     flush(stdout)
   end
-  return problem
 end
 
-function alternating_update(sweep_iterator; outputlevel=0, kwargs...)
+function alternating_update(
+  sweep_iterator;
+  outputlevel=0,
+  region_callback=default_region_callback,
+  sweep_callback=default_sweep_callback,
+  sweep_printer=default_sweep_printer,
+  kwargs...,
+)
   for (sweep, region_iter) in enumerate(sweep_iterator)
     prob = problem(region_iter)
     for (region, region_kwargs) in region_tuples(region_iter)
+      region_callback(
+        prob;
+        nsweeps=length(sweep_iterator),
+        outputlevel,
+        region,
+        region_kwargs,
+        sweep,
+        kwargs...,
+      )
     end
-    prob = sweep_callback(
-      prob; nsweeps=length(sweep_iterator), outputlevel, sweep, kwargs...
-    )
-    #TODO need to update `problem` reference held within region iterator(s)
+    sweep_callback(prob; nsweeps=length(sweep_iterator), outputlevel, sweep, kwargs...)
+    sweep_printer(prob; nsweeps=length(sweep_iterator), outputlevel, sweep, kwargs...)
   end
   return problem(last(sweep_iterator))
 end
