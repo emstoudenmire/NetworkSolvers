990e88a6406d3458d78c5fa7e8bab00a2534d0d3
Miles <miles.stoudenmire@gmail.com>
2025-04-04 22:28:28 -0400

    Partial work on QN subspace code

--- src/subspace.jl
+++ src/subspace.jl
@@ -4,16 +4,49 @@ using ITensors:
   dim,
   directsum,
   dot,
+  flux,
   hascommoninds,
   Index,
+  inds,
   norm,
   onehot,
+  pause,
+  space,
   uniqueinds,
-  random_itensor
+  random_itensor,
+  qr
 
-# TODO: hoist num_expand default value out to a function or similar
+expand_space(χ::Integer, expansion_factor) = max(χ+1,floor(Int,expansion_factor*χ))
+
+function expand_space(χs::Vector{<:Pair}, expansion_factor)
+  return [q=>expand_space(d,expansion_factor) for (q,d) in χs]
+end
+
+total_space(spaces::Vector{<:Integer}) = prod(spaces)
+
+function total_space(spaces::Vector)
+  merge_space = Dict{QN,Int}()
+  for it in Iterators.product(spaces)
+    qtot = QN()
+    dtot = 1
+    for qd in it
+      qtot += qd[1]
+      dtot *= qd[2]
+    end
+    merge_space[qtot] += dtot
+  end
+  @show merge_space
+  return [q=>d for (q,d) in merge_space]
+end
+
+function expand_space(a, basis_inds, expansion_factor)
+  basis_space = total_space(space.(basis_inds))
+  @show basis_space
+  max_expand = dim_basis - dim(a)
+end
+
 function subspace_expand!(
-  problem::EigsolveProblem, local_tensor, region; prev_region, num_expand=4, kws...
+  problem::EigsolveProblem, local_tensor, region; prev_region, expansion_factor=1.1, kws...
 )
   if isnothing(prev_region) || isa(region, AbstractEdge)
     return local_tensor
@@ -37,22 +70,23 @@ function subspace_expand!(
   isnothing(a) && return local_tensor
   basis_inds = uniqueinds(A, C)
 
+  #------------------------
+
   # Determine maximum value of num_expand
-  dim_basis = prod(dim.(basis_inds))
-  num_expand = min(num_expand, dim_basis - dim(a))
-  (num_expand <= 0) && return local_tensor
+  #dim_basis = prod(dim.(basis_inds))
+  #max_expand = dim_basis - dim(a)
 
-  # Build new subspace
-  function linear_map(w)
-    return w = w - A * (dag(A) * w)
-  end
-  random_vector() = random_itensor(basis_inds...)
-  Q = range_finder(linear_map, random_vector; max_rank=num_expand, oversample=0)
+  ax_space = expand_space(a,basis_inds,expansion_factor)
+  pause()
+  ax = Index(ax_space,"ax")
+  #(num_expand <= 0) && return local_tensor
 
-  # Direct sum new space with A to make Ax
-  qinds = [Index(1, "q$j") for j in 1:num_expand]
-  Q = [Q[j] * onehot(qinds[j] => 1) => qinds[j] for j in 1:num_expand]
-  Ax, sa = directsum(A => a, Q...)
+  linear_map(w) = (w - A * (dag(A) * w))
+
+  Y = linear_map(random_itensor(basis_inds,ax))
+  Q,R = qr(Y,basis_inds)
+  q = commonind(Q,R)
+  Ax, sa = directsum(A => a, Q => q)
 
   expander = dag(Ax) * A
   psi[prev_vertex] = Ax
