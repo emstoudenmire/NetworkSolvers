c6f65abfec751ad85f24b7d2d1185963fa00957d
Miles <miles.stoudenmire@gmail.com>
2025-05-15 22:27:13 -0400

    Update tdvp example

--- examples/tdvp.jl
+++ examples/tdvp.jl
@@ -21,19 +21,22 @@ function tdvp(; N=10, total_time=1.0, time_step=0.1)
 
   outputlevel = 0
   nsites = 1
-  truncation_kwargs = (; maxdim=16, cutoff=1E-10, normalize=true)
+  inserter_kwargs = (; maxdim=16, cutoff=1E-10, normalize=true)
   time_range = 0.0:time_step:total_time
-  res_1site = ns.tdvp(H, psi0, time_range; nsites, truncation_kwargs, outputlevel)
+  res_1site = ns.tdvp(H, psi0, time_range; nsites, inserter_kwargs, outputlevel)
 
   # Using RK solver
   updater_kwargs = (; solver=ns.runge_kutta_solver, order=4)
   res_rk4 = ns.tdvp(
-    H, psi0, 0.0:time_step:total_time; truncation_kwargs, updater_kwargs, outputlevel
+    H, psi0, 0.0:time_step:total_time; inserter_kwargs, updater_kwargs, outputlevel
   )
 
   # Using 2-site sweeping scheme
   nsites = 2
-  res_2site = ns.tdvp(H, psi0, time_range; nsites, truncation_kwargs, outputlevel)
+  updater_kwargs = (; solver=ns.runge_kutta_solver, order=4)
+  res_2site = ns.tdvp(
+    H, psi0, time_range; nsites, inserter_kwargs, updater_kwargs, outputlevel
+  )
 
   @show inner(res_1site, res_2site)
   @show inner(res_1site, res_rk4)
@@ -42,7 +45,7 @@ function tdvp(; N=10, total_time=1.0, time_step=0.1)
   return nothing
 end
 
-function test_tdvp(; N=6, total_time=0.1, time_step=0.01)
+function test_tdvp(; N=4, total_time=0.1, time_step=0.01)
   Random.seed!(1)
   s = itn.siteinds("S=1", N)
 
@@ -53,31 +56,49 @@ function test_tdvp(; N=6, total_time=0.1, time_step=0.01)
     os += 0.5, "S-", j, "S+", j + 1
   end
   H = itn.mpo(os, s)
-  psi = itn.random_mps(s; link_space=32)
+  psi = itn.random_mps(s; link_space=64)
 
   time_range = 0.0:time_step:total_time
   nsweeps = length(time_range) - 1
+
   szs = zeros(nsweeps, N)
-  function region_callback(problem; sweep, kws...)
-    return szs[sweep, :] = itn.expect(problem.state, "Sz")
+  function sweep_callback(problem; sweep, kws...)
+
+    #sz_t_expect = itn.expect(problem.state, "Sz")
+
+    psix = prod(problem.state)
+    psix /= norm(psix)
+    sz_t = Float64[]
+    for j in 1:N
+      val = real(scalar(dag(prime(psix, s[j])) * itm.op("Sz", s[j]) * psix))
+      push!(sz_t, val)
+    end
+
+    return szs[sweep, :] = sz_t
   end
 
   outputlevel = 0
-  truncation_kwargs = (; maxdim=100, cutoff=1E-10)
-  updater_kwargs = (; solver=ns.runge_kutta_solver, order=2)
-  subspace_kwargs = (; algorithm="densitymatrix", maxdim=4)
-  #subspace_kwargs = (;)
+  inserter_kwargs = (; maxdim=500, cutoff=1E-14, normalize=true)
+
+  updater_kwargs = (; solver=ns.runge_kutta_solver, order=4)
+  #updater_kwargs = (; solver=ns.exponentiate_solver)
+
+  #subspace_kwargs = (; algorithm="densitymatrix", maxdim=4)
+  subspace_kwargs = (;)
+
+  nsites = 2
+
   res = ns.applyexp(
     H,
     psi,
     time_range;
-    truncation_kwargs,
+    nsites,
+    inserter_kwargs,
     outputlevel,
-    region_callback,
+    sweep_callback,
     subspace_kwargs,
     updater_kwargs,
   )
-  #@show norm(res)
 
   println("\nResult from TDVP:")
   display(szs)
@@ -91,9 +112,9 @@ function test_tdvp(; N=6, total_time=0.1, time_step=0.01)
   szs_ed = zeros(nsweeps, N)
   for sweep in 1:nsweeps
     psi = noprime(psi * expH)
+    psi /= norm(psi)
     for j in 1:N
-      szs_ed[sweep, j] =
-        scalar(dag(prime(psi, s[j])) * itm.op("Sz", s[j]) * psi) / norm(psi)
+      szs_ed[sweep, j] = real(scalar(dag(prime(psi, s[j])) * itm.op("Sz", s[j]) * psi))
     end
   end
   println("\nResult from ED:")
